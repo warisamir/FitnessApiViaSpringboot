30bd74e48479b58fca1344c37f481eaddff679b7
warisamir <warisamir1918@gmail.com>
2025-07-06 16:07:53 +0530

    process data from ai

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
@@ -7,6 +7,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -30,12 +35,75 @@ public class ActivityAIService {
                     .replaceAll("```json\\n", "")  // removes ```json with any linebreak or whitespace
                     .replaceAll("`\\n``", "")              // removes closing ```
                     .trim();
-            log.info("PARSED Response from AI :{} ",jsonContent);
+//            log.info("PARSED Response from AI :{} ",jsonContent);
+            JsonNode analysisJson=mapper.readTree(jsonContent);
+            JsonNode analysisNode= analysisJson.path("analysis");
+            StringBuilder fullAnalysis=new StringBuilder();
+            addAnaylsisSection(fullAnalysis,analysisNode,"overall","OverAll:");
+            addAnaylsisSection(fullAnalysis,analysisNode,"pace","Pace:");
+            addAnaylsisSection(fullAnalysis,analysisNode,"heartRate","Heart Rate:");
+            addAnaylsisSection(fullAnalysis,analysisNode,"caloriesBurnt","Calories Burnt:");
+            JsonNode ImprovementNode= analysisJson.path("improvements");
+            JsonNode suggestionNode=analysisJson.path("suggestions");
+            JsonNode safetyguide=analysisJson.path("safety");
+            List<String> suggestions = extractListFromJson(suggestionNode, "workout", "description", "No specific suggestions provided");
+            List<String> improvements = extractListFromJson(ImprovementNode, "area", "recommendation", "No specific improvements provided");
+            List<String> safetyGuideLines= extractSafetyGuideline(safetyguide);
         }
         catch(Exception e){
             e.printStackTrace();
         };
     }
+
+    private List<String> extractSafetyGuideline(JsonNode safetyNode) {
+        List<String>safety=new ArrayList<>();
+        if(safetyNode.isArray()){
+            safetyNode.forEach(safetyn->safety.add(safetyn.asText()));
+        }
+        return safety.isEmpty()?Collections.singletonList("Follow general safety Guidelines"):safety;
+    }
+//    private List<String> extractSuggestions(JsonNode SuggestionNode) {
+//        List<String> Suggestions = new ArrayList<>();
+//        if (SuggestionNode.isArray()) {
+//            SuggestionNode.forEach(improvement -> {
+//                String area = improvement.path("workout").asText();
+//                String detail = improvement.path("description").asText();
+//                Suggestions.add(String.format("%s,%s", area, detail));
+//            });
+//        }
+//        return Suggestions.isEmpty() ? Collections.singletonList("No specific Suggestion provided") : Suggestions;
+//    }
+//    private List<String> extractImprovements(JsonNode improvementNode) {
+//        List<String> improvements = new ArrayList<>();
+//        if (improvementNode.isArray()) {
+//            improvementNode.forEach(improvement -> {
+//                String area = improvement.path("area").asText();
+//                String detail = improvement.path("recommendation").asText();
+//                improvements.add(String.format("%s,%s", area, detail));
+//            });
+//        }
+//        return improvements.isEmpty() ? Collections.singletonList("No specific improvements provided") : improvements;
+//    }
+//
+    private List<String> extractListFromJson(JsonNode node, String field1, String field2, String defaultMsg) {
+        List<String> result = new ArrayList<>();
+        if (node != null && node.isArray()) {
+            node.forEach(item -> {
+                String val1 = item.path(field1).asText();
+                String val2 = item.path(field2).asText();
+                result.add(String.format("%s,%s", val1, val2));
+            });
+        }
+        return result.isEmpty() ? Collections.singletonList(defaultMsg) : result;
+    }
+
+    private void addAnaylsisSection(StringBuilder fullAnalysis, JsonNode analysisNode, String key, String prefix) {
+        if(!analysisNode.path(key).isMissingNode()){
+            fullAnalysis.append(prefix).append(analysisNode.path(key).asText()).append("\n\n");
+        }
+    }
+
+
     private String createPromptforactivity(Activity activity){
         return String.format("""
         Analyze this fitness  and provide detailed recommendations in this format:
